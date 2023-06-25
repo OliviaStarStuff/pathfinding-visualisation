edc1c3276baf68d3dc93397b0ad6d6a26782b927
Olivia <104592012+OliviaStarStuff@users.noreply.github.com>
2023-06-25 16:18:32 +0100

    Refactor app_double

--- app_double.py
+++ app_double.py
@@ -2,16 +2,12 @@
 import pygame as pg
 from grid import HexGrid
 from gameController import GameController
-from agents import Node
-from queue import PriorityQueue
+from mainController import mainController
 
 # pg setup
 pg.init()
 screen = pg.display.set_mode((1280, 720))
-SCREEN_SIZE = pg.Vector2(screen.get_width(), screen.get_height())
-CENTER = SCREEN_SIZE / 2
-player_pos = CENTER.copy()
-TEXT_LEFT_ANCHOR = 20
+CENTER = pg.Vector2(screen.get_width(), screen.get_height()) / 2
 
 # create grid
 size = 30
@@ -22,7 +18,6 @@ grid = HexGrid(dimensions, grid_pos, size=size)
 # create walls
 goal_pos = grid.random_pos()
 wall_num = int(grid.get_size()*0.2)
-grid.generate_walls(wall_num, grid.pos_to_coords(goal_pos), grid.pos_to_coords(player_pos))
 controller = GameController(screen, grid, goal_pos, "bestFirst",
                             is_random_start=True, change_agent_type=False,
                             wait_time=2500, wall_num=wall_num)
@@ -34,7 +29,11 @@ controller2 = GameController(screen, grid2, goal_pos, "astar",
                              is_random_start=True, change_agent_type=False,
                              wait_time=2500, wall_num=wall_num)
 
-myfont = pg.font.SysFont('Consolas', 16)
+controller2.grid.cells = controller.grid.copy()
+controller2.agent.reset_open()
+
+font = pg.font.SysFont('Consolas', 16)
+main_controller = mainController(screen, font, [controller, controller2], False)
 
 clock = pg.time.Clock()
 running = True
@@ -48,77 +47,10 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((0,0,16))
     dt = clock.tick(40)
-    controller.update(screen, dt)
-    controller2.update(screen, dt)
 
-    if controller.wait_timer != controller2.wait_timer:
-        wait_time = min(controller.wait_timer, controller2.wait_timer)
-        controller.wait_timer = wait_time
-        controller2.wait_timer = wait_time
-    if controller.has_reset or controller2.has_reset:
-        c, c2 = controller2, controller
-        if controller.has_reset:
-            c, c2 = controller, controller
-        c.goal.pos = c2.goal.pos.copy()
-        # c.reset()
-        c.wait_timer = 0
-        c.player.pos = c2.player.pos.copy()
-        c.agent.open = PriorityQueue()
-        c.grid.cells = c2.grid.copy()
-        c.has_reset = False
-        estimated_cost = c.grid.estimate_remaining_cost(
-            c.goal.pos, c.player.pos)
-        c.agent.open.put(Node(c.player.pos, None, 0, estimated_cost, 0, c.agent.selected))
-        c.player.reset_trail(c.grid.pos_to_coords(c.player.pos))
-
-   # print current trail
-    current = controller.agent.current
-    trail_counter = 1
-    while current is not None and controller.agent.has_goal is not True:
-        p2 = []
-        for p in controller.grid.get_points():
-            p2.append(p + controller.grid.pos_to_coords(current.pos))
-        pg.gfxdraw.filled_polygon(screen, p2, pg.Color('orange'))
-        current = current.parent
-        trail_counter += 1
-
-   # print current trail
-    current = controller2.agent.current
-    trail_counter = 1
-    while current is not None and controller2.agent.has_goal is not True:
-        p2 = []
-        for p in controller2.grid.get_points():
-            p2.append(p + controller2.grid.pos_to_coords(current.pos))
-        pg.gfxdraw.filled_polygon(screen, p2, pg.Color('orange'))
-        current = current.parent
-        trail_counter += 1
-
-    for i, node in enumerate(controller.agent.open.queue):
-        text = myfont.render(f"{node.total_cost:.0f}", 1, (255, 255, 255))
-        screen.blit(text,
-                    controller.grid.pos_to_coords(node.pos)
-                    - pg.Vector2(text.get_width()//2-1,text.get_height()//2-2))
-
-    for i, node in enumerate(controller2.agent.open.queue):
-        text = myfont.render(f"{node.total_cost:.0f}", 1, (255, 255, 255))
-        screen.blit(text,
-                    controller2.grid.pos_to_coords(node.pos)
-                    - pg.Vector2(text.get_width()//2-1,text.get_height()//2-2))
-
-    # Credits
-    message = "Created by Olivia, CompSci BSC 2022/2023 University of Sheffield"
-    text = myfont.render(message, 1, (255, 0, 0))
-    screen.blit(text, CENTER + pg.Vector2(-text.get_width()//2, CENTER.y-26))
-
-    # Write algorithm title.
-    text = myfont.render(controller.agent.selected.upper(), 1, (255, 0, 0))
-    screen.blit(text,  pg.Vector2(controller.grid.center.x-text.get_width(), 20))
-
-    # Write algorithm title.
-    text = myfont.render(controller2.agent.selected.upper(), 1, (255, 0, 0))
-    screen.blit(text,  pg.Vector2(controller2.grid.center.x-text.get_width(), 20))
+    main_controller.update(dt)
+    controller.sync_with(controller2)
 
     pg.display.flip()
-    # dt = clock.tick(40)
 
 pg.quit()
--- square_app_double.py
+++ square_app_double.py
@@ -2,23 +2,20 @@
 import pygame as pg
 from sq_grid import SquareGrid
 from gameController import GameController
+from mainController import mainController
 
 # pg setup
 pg.init()
 screen = pg.display.set_mode((1280, 720))
-SCREEN_SIZE = pg.Vector2(screen.get_width(), screen.get_height())
-CENTER = SCREEN_SIZE / 2
-player_pos = CENTER.copy()
-TEXT_LEFT_ANCHOR = 20
+CENTER = pg.Vector2(screen.get_width(), screen.get_height()) / 2
 
 # create grid
 size = pg.Vector2(30,30)
 grid_pos = CENTER-pg.Vector2(17*15, 0)
 grid = SquareGrid(pg.Vector2(15, 20), grid_pos, size=size)
-# create walls
 goal_pos = grid.random_pos()
+
 wall_num = int(grid.get_size()*0.07)
-grid.generate_walls(wall_num, grid.coords_to_pos(goal_pos), grid.coords_to_pos(player_pos))
 controller = GameController(screen, grid, goal_pos, "bestFirst",
                             is_random_start=False, change_agent_type=False,
                             wait_time=2500, wall_num=wall_num)
@@ -26,11 +23,16 @@ controller = GameController(screen, grid, goal_pos, "bestFirst",
 grid_pos = CENTER+pg.Vector2(17*15, 0)
 grid2 = SquareGrid(pg.Vector2(15, 20), grid_pos, size=size)
 grid2.cells = grid.copy()
+
 controller2 = GameController(screen, grid2, goal_pos, "astar",
                              is_random_start=False, change_agent_type=False,
                              wait_time=2500, wall_num=wall_num)
 
-myfont = pg.font.SysFont('Consolas', 16)
+controller2.grid.cells = controller.grid.copy()
+controller2.agent.reset_open()
+
+font = pg.font.SysFont('Consolas', 16)
+main_controller = mainController(screen, font, [controller, controller2], False)
 
 clock = pg.time.Clock()
 running = True
@@ -44,66 +46,10 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((0,0,16))
     dt = clock.tick(60)
-    controller.update(screen, dt)
-    controller2.update(screen, dt)
 
+    main_controller.update(dt)
     controller.sync_with(controller2)
 
-
-
-   # print current trail
-    current = controller.agent.current
-    trail_counter = 1
-    while current is not None and controller.agent.has_goal is not True:
-        coords = controller.grid.pos_to_coords(current.pos)
-        coords -= controller.grid.size/2
-        dimensions = pg.Rect(coords.x, coords.y, controller.grid.size.x, controller.grid.size.y)
-        pg.draw.rect(screen, pg.Color('orange'), dimensions)
-        current = current.parent
-        trail_counter += 1
-
-
-
-   # print current trail
-    current = controller2.agent.current
-    trail_counter = 1
-    while current is not None and controller2.agent.has_goal is not True:
-        coords = controller2.grid.pos_to_coords(current.pos)
-        coords -= controller2.grid.size/2
-        dimensions = pg.Rect(coords.x, coords.y, controller2.grid.size.x, controller2.grid.size.y)
-        pg.draw.rect(screen, pg.Color('orange'), dimensions)
-        current = current.parent
-        trail_counter += 1
-
-    for i, node in enumerate(controller.agent.open.queue):
-        text = myfont.render(f"{node.global_cost/30/30:.0f}", 1, (255, 255, 255))
-        screen.blit(text,
-                    controller.grid.pos_to_coords(node.pos)
-                    - pg.Vector2(text.get_width()//2-1,text.get_height()//2-2))
-
-    for i, node in enumerate(controller2.agent.open.queue):
-        text = myfont.render(f"{node.global_cost/30/30:.0f}", 1, (255, 255, 255))
-        screen.blit(text,
-                    controller2.grid.pos_to_coords(node.pos)
-                    - pg.Vector2(text.get_width()//2-1,text.get_height()//2-2))
-
-
-    # draw axes
-
-    # Credits
-    message = "Created by Olivia, CompSci BSC 2022/2023 University of Sheffield"
-    text = myfont.render(message, 1, (255, 0, 0))
-    screen.blit(text, CENTER + pg.Vector2(-text.get_width()//2, CENTER.y-26))
-
-    # Write algorithm title.
-    text = myfont.render(controller.agent.selected.upper(), 1, (255, 0, 0))
-    screen.blit(text,  pg.Vector2(controller.grid.center.x-text.get_width(), 20))
-
-    # Write algorithm title.
-    text = myfont.render(controller2.agent.selected.upper(), 1, (255, 0, 0))
-    screen.blit(text,  pg.Vector2(controller2.grid.center.x-text.get_width(), 20))
-
     pg.display.flip()
-    # dt = clock.tick(40)
 
 pg.quit()
